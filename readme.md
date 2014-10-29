a5503fc8c670ffbb3fd4e0c961184c0430e0588e
Tooblippe <tobie.nortje@gmail.com>
2014-10-29 18:39:54 +0200

    update , cleanup

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Scrape Power Station Info from Website
+
+Scrapes some information of the [globalenergyobservatory.org](http://globalenergyobservatory.org/list.php?db=PowerPlants&type=Coal) site.
+
+# Dependancies
+
+* pandas
+
+* lxml
+
+* requests
+
--- scrape_site.py
+++ scrape_site.py
@@ -7,6 +7,7 @@ Created on Wed Oct 22 19:36:30 2014
 """
 import pandas as pd
 import requests
+
 from lxml import html
 
 
@@ -15,15 +16,14 @@ data = pd.DataFrame()
 #empty list for temp use
 el = []
 
-
-scrape_url = 'http://globalenergyobservatory.org/list.php?db=PowerPlants&type=Coal'
 base_url = 'http://globalenergyobservatory.org/'
+scrape_url = 'http://globalenergyobservatory.org/list.php?db=PowerPlants&type=Coal'
 
 
 #read all the links to dataframe data
 #-------------------------------------
-page = requests.get(scrape_url)
-dom = html.fromstring(page.text)
+page  = requests.get(scrape_url)
+dom   = html.fromstring(page.text)
 
 for link in dom.xpath('//a/@href'): # select the url in href for all a tags(links)
     if 'geoid' in link:
@@ -33,6 +33,9 @@ data['url'] = el
 
 
 #transverse all pages and save complete page to dataframe
+#we should do this in parralel using map
+#puts everything in a dataframe...not sure this is good iea
+#idea was to pickle the dataframe...but did not work
 #----------------------------------------------------------
 el = []
 for i, station_url in enumerate(data['url']):
@@ -45,14 +48,19 @@ data['dom'] = el
 
 
 #parse dom to find info
-#-----------------------
+#uses xpath to get some info
+#-scrape from DOM
 data['description'] = data['dom'].apply(lambda x : x.xpath('//*[@class="wrapper"]/form[1]/div[1]/table[2]/tr/td/text()')[0])
-data['units'] = data['description'].apply( lambda a : a[a.find('It has')+7:a.find('It has')+9].rstrip() )
-data['plant_type'] = data['description'].apply( lambda a :a[a.find('TYPE')+5:a.find('with')].rstrip())
-data['capacity'] = data['description'].apply( lambda a:a[a.find('capacity of')+12:a.find('MWe')].rstrip())
-data['operated_by'] = data['description'].apply( lambda a : a[a.find('operated by')+12:-1].rstrip() )
-data['name'] = data['dom'].apply(lambda x : x.xpath('//*[@id="Name"]/@value')[0])
-data['boiler'] = data['dom'].apply(lambda x : x.xpath(' //*[@id="Boiler_Manufacturer_1"]/@value')[0])
+data['name'] 		= data['dom'].apply(lambda x : x.xpath('//*[@id="Name"]/@value')[0])
+data['boiler'] 		= data['dom'].apply(lambda x : x.xpath(' //*[@id="Boiler_Manufacturer_1"]/@value')[0])
+
+
+#scrape from description tag using pythons text functions
+#offsets hard coded. not a good idea should format change
+data['units'] 		= data['description'].apply( lambda a : a[a.find('It has') + 7:a.find('It has')+9].rstrip() )
+data['plant_type']  = data['description'].apply( lambda a : a[a.find('TYPE') + 5:a.find('with')].rstrip())
+data['capacity'] 	= data['description'].apply( lambda a : a[a.find('capacity of') + 12:a.find('MWe')].rstrip())
+data['operated_by'] = data['description'].apply( lambda a : a[a.find('operated by')+ 12 :-1].rstrip() )
 
 
 #prepare xls writeout
@@ -67,4 +75,6 @@ writeout['boiler']          = data['boiler']
 writeout['operated_by']     = data['operated_by']
 writeout['url']             = data['url']
 
+# write out to excel -- the needed format
+# you can use .to_csv() to be more portable
 writeout.to_excel('power_scrape.xls')
